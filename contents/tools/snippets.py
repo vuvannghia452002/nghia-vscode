0f87d0bdb0da8df09f097746d23b4d3ef08c5365
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-04-19 00:43:05 +0700

    chore: update python.json

---
+++ contents/tools/snippets.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+
+def get_snippets_path():
+    if os.name == 'nt':
+        return os.path.expanduser('~/AppData/Roaming/Code/User/snippets')
+    # else:
+    #     return os.path.expanduser('~/.config/Code/User/snippets')
+
+
+source_folder = "../snippets"
+destination_folder = get_snippets_path()
+
+
+for root, dirs, files in os.walk(source_folder):
+    for file in files:
+        source_path = os.path.join(root, file)
+        destination_path = os.path.join(destination_folder, file)
+
+        shutil.copy2(source_path, destination_path)
+ngh
